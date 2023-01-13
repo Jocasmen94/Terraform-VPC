d75bb85a05bf6d224798a33ef49a2a332c9a6d58
jocasmen94 <javier_jocasmen94@hotmail.com>
2023-01-13 16:21:42 +0000

    change to local backend

--- main.tf
+++ main.tf
@@ -7,13 +7,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "jocasmen-terraform"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = "jocasmen-terraform"
+#    key            = "terraform.tfstate"
+#    region         = "us-east-1"
+#  }
+#}
 
 
 resource "aws_vpc" "main-1" {
