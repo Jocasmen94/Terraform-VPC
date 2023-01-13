f3d78052a95253c5d14c0e6b5ad41034bc66724a
jocasmen94 <javier_jocasmen94@hotmail.com>
2023-01-13 15:04:37 +0000

    vpc-subnets

--- main.tf
+++ main.tf
@@ -24,3 +24,59 @@ resource "aws_vpc" "main-1" {
     Name = "Main VPC 1"
   }
 }
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.main-1.id
+  cidr_block = "10.0.10.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Subnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "gw-1" {
+  vpc_id = aws_vpc.main-1.id
+
+  tags = {
+    Name = "GW-1"
+  }
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.main-1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw-1.id
+  }
+  
+  tags = {
+    Name = "Route-Table-1"
+  }
+}
+
+resource "aws_route_table_association" "table_subnet1" {
+  subnet_id = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.r.id
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
